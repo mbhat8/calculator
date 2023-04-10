7d2de0e2993eefd7bbb1c9be3beaf564b8aa4de8
mahesh <122031410+mbhat8@users.noreply.github.com>
2023-04-10 17:05:03 +0530

    Final Version

---
+++ script.js
@@ -0,0 +1,79 @@
+
+let operator="";
+let previousValue="";
+let currentValue="";
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed")
+  let clear=document.querySelector('#clear');
+  let operators=document.querySelectorAll('.operator');
+  let operand=document.querySelectorAll('.operand');
+  let decimal=document.querySelector('.decimal');
+  let equals=document.querySelector('.equal')
+
+  let previousScreen=document.querySelector('.previous')
+  let currentScreen=document.querySelector('.current')
+
+  operand.forEach((number)=>number.addEventListener("click",function(e){
+    handleNumber(e.target.textContent);
+    currentScreen.textContent=currentValue;
+  }))
+  
+  operators.forEach((op)=>op.addEventListener("click",function(e){
+    handleOp(e.target.textContent);
+    previousScreen.textContent=previousValue+" "+operator;
+    currentScreen.textContent=currentValue;
+  }))
+  
+  clear.addEventListener('click',function(){
+    operator="";
+    previousValue="";
+    currentValue="";
+    previousScreen.textContent=currentValue
+    currentScreen.textContent=currentValue
+  })
+
+  equals.addEventListener('click',function(){
+    calculate();
+    currentScreen.textContent=currentValue;
+  })
+})
+
+
+function handleNumber(num){
+  if(currentValue.length<=5){
+  currentValue+=num;
+  }
+}
+
+
+function handleOp(op){
+  operator=op;
+  previousValue=currentValue;
+  currentValue='';
+}
+
+
+function calculate(){
+  previousValue=Number(previousValue)
+  currentValue=Number(currentValue)
+
+  switch(operator){
+    case '+':currentValue=currentValue+previousValue;
+             break;
+    case '-':currentValue=currentValue-previousValue;
+             break;
+    case '*':currentValue=currentValue*previousValue;
+             break;
+    case '/':currentValue=previousValue/currentValue;
+             break;             
+    default:break;  
+  }
+  round(currentValue);
+}
+
+function round(num){
+  let newVal=Math.round((currentValue + Number.EPSILON) * 100) / 100;
+  newVal=newVal.toFixed(5);
+  currentValue=newVal;
+}
